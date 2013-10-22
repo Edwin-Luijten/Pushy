fbf7cb0e6fd23369f6cb9717ba709d38f5abf844
Edwin Luijten <edwin.luijten@live.nl>
2013-10-22 21:38:03 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+/**
+ * Configuration file for Pushy
+ */
+var config = {}
+
+config.app = {}
+config.db = {};
+
+/**
+ * App
+ */
+config.app.port = 3000;
+
+/**
+ * Database
+ */
+config.db.hostname = '';
+config.db.username = '';
+config.db.password = '';
+config.db.database = '';
+
+module.exports = config;
